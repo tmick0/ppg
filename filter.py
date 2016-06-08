961fe065bdca790fc09eae3bba2c58c32b170cac
lq0000 <root@lo.calho.st>
2016-06-08 02:21:04 -0600

    refactor nested loops to use itertools.product

--- filter.py
+++ filter.py
@@ -1,4 +1,5 @@
 import numpy as np
+from itertools import product
 from random import random, randint
 
 class AbstractFilter (object):
@@ -27,23 +28,21 @@ class PaethFilter (KernelFilter):
         else:
             copy = np.copy(data)
         height, width, channels = data.shape
-        for c in xrange(channels):
-            for j in xrange(1, height):
-                for i in xrange(1, width):
-                    A = data[j,   i-1, c]
-                    B = data[j-1, i,   c]
-                    C = data[j-1, i-1, c]
-                    p = A + B - C
-                    if abs(A - p) <= abs(B - p) and abs(A - p) <= abs(C- p):
-                        x = A
-                    elif abs(B - p) <= abs(C - p):
-                        x = B
-                    else:
-                        x = C
-                    if decode:
-                        copy[j, i, c] += x
-                    else:
-                        copy[j, i, c] -= x
+        for c, j, i in product(xrange(channels), xrange(1, height), xrange(1, width)):
+            A = data[j,   i-1, c]
+            B = data[j-1, i,   c]
+            C = data[j-1, i-1, c]
+            p = A + B - C
+            if abs(A - p) <= abs(B - p) and abs(A - p) <= abs(C- p):
+                x = A
+            elif abs(B - p) <= abs(C - p):
+                x = B
+            else:
+                x = C
+            if decode:
+                copy[j, i, c] += x
+            else:
+                copy[j, i, c] -= x
         return copy
 
 class SubFilter (KernelFilter):
@@ -54,14 +53,12 @@ class SubFilter (KernelFilter):
         else:
             copy = np.copy(data)
         height, width, channels = data.shape
-        for c in xrange(channels):
-            for j in xrange(1, height):
-                for i in xrange(1, width):
-                    A = data[j, i-1, c]
-                    if decode:
-                        copy[j, i, c] += A
-                    else:
-                        copy[j, i, c] -= A
+        for c, j, i in product(xrange(channels), xrange(1, height), xrange(1, width)):
+            A = data[j, i-1, c]
+            if decode:
+                copy[j, i, c] += A
+            else:
+                copy[j, i, c] -= A
         return copy
 
 class UniformGlitchFilter (NullFilter):
@@ -70,11 +67,9 @@ class UniformGlitchFilter (NullFilter):
         self.rate = rate
     
     def encode(self, data):
-        width, height, channels = data.shape
-        for c in xrange(channels):
-            for i in xrange(1, height):
-                for j in xrange(1, width):
-                    if random() < self.rate:
-                        data[j, i, c] = randint(0, 255)
+        height, width, channels = data.shape
+        for c, j, i in product(xrange(channels), xrange(height), xrange(width)):
+            if random() < self.rate:
+                data[j, i, c] = randint(0, 255)
         return data
     
