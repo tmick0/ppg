64c2693ba6eb50d4a89376d4b74b20dc6426c03c
lq0000 <root@lo.calho.st>
2016-06-08 02:13:53 -0600

    add sub filter

--- filter.py
+++ filter.py
@@ -14,12 +14,12 @@ class PaethFilter (AbstractFilter):
             copy = data
         else:
             copy = np.copy(data)
-        width, height, channels = data.shape
+        height, width, channels = data.shape
         for c in xrange(channels):
-            for i in xrange(1, height):
-                for j in xrange(1, width):
-                    A = data[j-1, i,   c]
-                    B = data[j,   i-1, c]
+            for j in xrange(1, height):
+                for i in xrange(1, width):
+                    A = data[j,   i-1, c]
+                    B = data[j-1, i,   c]
                     C = data[j-1, i-1, c]
                     p = A + B - C
                     if abs(A - p) <= abs(B - p) and abs(A - p) <= abs(C- p):
@@ -32,6 +32,30 @@ class PaethFilter (AbstractFilter):
                         copy[j, i, c] += x
                     else:
                         copy[j, i, c] -= x
+        return copy
+
+    def encode(self, data):
+        return self.kernel(data)
+    
+    def decode(self, data):
+        return self.kernel(data, decode=True)
+
+class SubFilter (AbstractFilter):
+
+    def kernel(self, data, decode=False):
+        if decode:
+            copy = data
+        else:
+            copy = np.copy(data)
+        height, width, channels = data.shape
+        for c in xrange(channels):
+            for j in xrange(1, height):
+                for i in xrange(1, width):
+                    A = data[j, i-1, c]
+                    if decode:
+                        copy[j, i, c] += A
+                    else:
+                        copy[j, i, c] -= A
         return copy
 
     def encode(self, data):
--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import sys
 import numpy as np
 from codec import SciKitCodec
-from filter import PaethFilter, UniformGlitchFilter
+from filter import PaethFilter, SubFilter, UniformGlitchFilter
 
 np.seterr(all='ignore')
 
 def main(infile, outfile):
-    codec = SciKitCodec()
-    paeth = PaethFilter()
-    glitch = UniformGlitchFilter(rate=0.00005)
+    codec  = SciKitCodec()
+    paeth  = PaethFilter()
+    sub    = SubFilter()
+    glitch = UniformGlitchFilter(rate=0.00002)
     
     f = codec.decode(infile)
+    
+    f = sub.encode(f)
+    f = glitch.encode(f)
+    f = glitch.encode(f)
+    f = sub.decode(f)
+    
     f = paeth.encode(f)
     f = glitch.encode(f)
     f = paeth.decode(f)
